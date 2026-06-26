d6cb32930178ae3370fbb31065b19aaf3ef9907b
plekhovgleb <venomancer11111@gmail.com>
2026-06-26 23:46:25 +0300

    added getMessage metod 3

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello qa.guru!NEW CHANCHES");
-        System.out.println("hello");
+        System.out.printf("Hello qa.guru!NEW CHANCHES1234213");
+        System.out.println("hello1321312");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
