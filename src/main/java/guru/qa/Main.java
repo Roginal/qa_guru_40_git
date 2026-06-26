735742fd5e8e22af543e590debc3698df8d185b1
plekhovgleb <venomancer11111@gmail.com>
2026-06-26 23:37:04 +0300

    added getMessage metod 2

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello qa.guru!NEW CHANCHES");
+        System.out.printf("Hello qa.guru!NEW CHANCHESssssss");
         System.out.println("hello");
 
         for (int i = 1; i <= 5; i++) {
@@ -16,6 +16,6 @@ public class Main {
         }
     }
     private static String getMessage () {
-        return "Hello qa_guru"
+        return "Hello qa_guru11111"
     }
 }
